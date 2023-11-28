13fbd93ccd8f656995444851d50af25e6686ffd4
Ben <Bendavcorr@Gmail.com>
2023-11-28 15:13:44 +0000

    .

--- main.py
+++ main.py
@@ -2,9 +2,30 @@ from flask import Flask
 from flask import request
 import json
 
-@app.route("/") #Default - Show Data
-def test:
-    return "Hello world"
+def create_app():
+  app = Flask(__name__)
+  # Error 404 handler
+  @app.errorhandler(404)
+  def resource_not_found(e):
+    return jsonify(error=str(e)), 404
+  # Error 405 handler
+  @app.errorhandler(405)
+  def resource_not_found(e):
+    return jsonify(error=str(e)), 405
+  # Error 401 handler
+  @app.errorhandler(401)
+  def custom_401(error):
+    return Response("API Key required.", 401)
+  
+  @app.route("/ping")
+  def hello_world():
+     return "pong"
+  
+  return app
+  
+app = create_app()
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',port='8080') #Run the flask app at port 8080
+  #    app = create_app()
+  print(" Starting app...")
+  app.run(host="0.0.0.0", port=8080)
