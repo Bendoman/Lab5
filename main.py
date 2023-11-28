22d467cbf4b7400349f61573961f11b999ba98da
Ben <Bendavcorr@Gmail.com>
2023-11-28 19:25:30 +0000

    .

--- main.py
+++ main.py
@@ -1,27 +1,50 @@
 import os
+import config
+import json
+import time
 from flask import Flask, jsonify, Response
 
 def create_app():
-  app = Flask(__name__)
-  # Error 404 handler
-  @app.errorhandler(404)
-  def resource_not_found(e):
+    app = Flask(__name__)
+    # Error 404 handler
+    @app.errorhandler(404)
+    def resource_not_found(e):
     return jsonify(error=str(e)), 404
-  # Error 405 handler
-  @app.errorhandler(405)
-  def resource_not_found(e):
+    # Error 405 handler
+    @app.errorhandler(405)
+    def resource_not_found(e):
     return jsonify(error=str(e)), 405
-  # Error 401 handler
-  @app.errorhandler(401)
-  def custom_401(error):
+    # Error 401 handler
+    @app.errorhandler(401)
+    def custom_401(error):
     return Response("API Key required.", 401)
+
+    @app.route("/ping")
+    def hello_world():
+        return "pong"
   
-  @app.route("/ping")
-  def hello_world():
-     return "pong"
   
-  return app
-  
+    @app.route("/version", methods=["GET"], strict_slashes=False)
+    def version():
+    response_body = {
+        "success": 1,
+    }
+    return jsonify(response_body)
+    
+    @app.after_request
+    def after_request(response):
+        if response and response.get_json():
+            data = response.get_json()
+
+            data["time_request"] = int(time.time())
+            data["version"] = config.VERSION
+
+            response.set_data(json.dumps(data))
+
+        return response
+    
+    return app  
+
 app = create_app()
 
 if __name__ == "__main__":
