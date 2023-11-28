dce27876465a77447b29944b5ab134d999003ac6
Ben <Bendavcorr@Gmail.com>
2023-11-28 15:31:37 +0000

    .

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
 from flask import Flask
 from flask import request
+from flask_mysqldb import MySQL
+from flask_cors import CORS
 import json
 
 def create_app():
   app = Flask(__name__)
-  # Error 404 handler
-  @app.errorhandler(404)
-  def resource_not_found(e):
-    return jsonify(error=str(e)), 404
-  # Error 405 handler
-  @app.errorhandler(405)
-  def resource_not_found(e):
-    return jsonify(error=str(e)), 405
-  # Error 401 handler
-  @app.errorhandler(401)
-  def custom_401(error):
-    return Response("API Key required.", 401)
-  
-  @app.route("/ping")
+
+  @app.route("/")
   def hello_world():
-     return "pong"
-  
+     return "Hello world"
+
   return app
   
 app = create_app()
